975c22699a9caad26883f9b019e4324681779b7b
hiren <hiren.techvivanta@outlook.com>
2025-12-10 12:41:59 +0530

    add scroll to top section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { BrowserRouter, Routes, Route,  ScrollRestoration  } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Blogs from './pages/Blogs';
-import Contact from './pages/Contact';
-import Terms from './pages/Terms';
-import Privacy from './pages/Privacy';
-
-// ScrollToTop component to reset scroll on route change
-const ScrollToTop = () => {
-    // This functionality is now handled by ScrollRestoration from react-router-dom in v6.4+ data routers, 
-    // or we can use a simple useEffect hook, but BrowserRouter doesn't support data APIs by default in the same way.
-    // However, including ScrollRestoration inside Routes isn't right. 
-    // Let's stick to simple implementation or use a wrapper.
-    // Actually, with react-router-dom v6, we can just use a useEffect in a wrapper.
-    // Restoring scroll is good UX.
-    return null; 
-};
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  ScrollRestoration,
+} from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import Terms from "./pages/Terms";
+import Privacy from "./pages/Privacy";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Scroll Restoration handled manually or via simple useEffect if needed, but for now default behavior is ok-ish, 
-          though standard is to scroll to top. 
-          Let's add a simple ScrollToTop if we had a util, but I'll skip complexity for now unless requested. 
-      */}
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
